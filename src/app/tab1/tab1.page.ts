c294d51b5321785a0fabd409647cd712e718c37e
Midori Sugaya <sugaya.midori@gmail.com>
2023-11-05 17:00:50 -0300

    criação do app Pokemon

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
+import { viaCEPService } from './../services/via-cep.service';
+import { PokeAPIService } from './../services/poke-api.service';
 import { Component } from '@angular/core';
+import { ValueAccessor } from '@ionic/angular/common';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf:''
+  };
+  
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: viaCEPService,
+  ) {}
+  buscarPokemon(){
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe ((value) => {
+      this.areaBusca.logradouro = JSON.parse (JSON.stringify(value)) ['logradouro'];
+    this.areaBusca.bairro = ',' + JSON.parse (JSON.stringify(value)) ['bairro'];
+    this.areaBusca.localidade = ',' + JSON.parse (JSON.stringify(value)) ['localidade'];
+    this.areaBusca.uf = ',' + JSON.parse (JSON.stringify(value)) ['uf'];
+    });   
+  this.pokeAPIService.getPokeAPIService();
+}
 }
