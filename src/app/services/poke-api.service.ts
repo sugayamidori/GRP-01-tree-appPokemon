9764959d4a6e9832aba4d60c7f9876a7202c5c69
Midori Sugaya <sugaya.midori@gmail.com>
2023-11-07 15:33:21 -0300

    adicionado atividades da tab2 com erro na comparação das habilidades (dando empate)
    COMANDOS EXECUTADOS - Lista 6
    
    CRIAÇÃO DE SERVIÇO:
    touch src/app/services/photo.service.ts
    
    CAPACITOR:
    npx cap doctor
    npm install @capacitor/core@latest
    npx cap update
    
    ATIVAÇÃO DA CÂMERA:
    npm install @ionic/pwa-elements
    
    import { defineCustomElements } from '@ionic/pwa-elements/loader';
    
    defineCustomElements(window);
    if (environment.production) {
      enableProdMode();
    }
    
    SERVIÇO PARA COMPARTILHAR DADO:
    touch src/app/services/shared-data.service.ts

--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -8,6 +8,6 @@ import { Injectable } from "@angular/core";
 export class PokeAPIService {
     constructor (private httpCliente: HttpClient) {}
     getPokeAPIService (id: number = Math.floor (Math.random()*100)){
-        return this.httpCliente.get ('https://pokeapi.co/api/v2/pokemon/${id}');
+        return this.httpCliente.get (`https://pokeapi.co/api/v2/pokemon/${id}`);
     }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,17 @@
 import { viaCEPService } from './../services/via-cep.service';
 import { PokeAPIService } from './../services/poke-api.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ValueAccessor } from '@ionic/angular/common';
 import { __values } from 'tslib';
+import { ShareDataService } from '../services/shared-data.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  numberOfAbilitiesTab1: any;
   areaBuscarPokemon: string = '52011210';
   areaBusca: any = {
     bairro: '',
@@ -18,10 +20,12 @@ export class Tab1Page {
     uf:''
   };
   
-  constructor(
-    private pokeAPIService: PokeAPIService,
-    private viaCEPService: viaCEPService,
-  ) {}
+  constructor(private pokeAPIService: PokeAPIService, private viaCEPService: viaCEPService, private shareDataService: ShareDataService){}
+
+  ngOnInit(): void {
+    this.numberOfAbilitiesTab1 = 2;
+    this.shareDataService.setNumberOfAbilitiesTab1(this.numberOfAbilitiesTab1);
+  }
   buscarPokemon(){
     this.viaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe ((value) => {
       this.areaBusca.logradouro = JSON.parse (JSON.stringify(value)) ['logradouro'];
